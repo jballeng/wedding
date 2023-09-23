721d444c2b26e3e4053ff647c4a005a35a208304
Jake Ballenger <71456963+jballeng@users.noreply.github.com>
2023-09-23 12:17:13 -0400

    Mobile FAQ Fix and meta

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
   return (
@@ -9,28 +8,20 @@ export default function Meta() {
         sizes="180x180"
         href="/favicon/icon.webp"
       />
-     
-      
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href="/favicon/icon.webp"
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/icon.webp" />
       <meta name='robots' content='noindex' />
       <meta name='googlebot' content='noindex' />
-      
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta name='title' content="Jake and Reagan's Wedding Website" />
-
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} key="ogImage" />
       <title>Jake and Reagan's Wedding Website</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
     </Head>
   )
